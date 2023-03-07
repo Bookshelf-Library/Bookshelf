26dbbe5bf18c22bb1a27562cd3cb7669d8ba0e81
Igorttdp <igorttdp.dev@email.com>
2023-03-07 20:40:09 -0300

    feat: add generic view for create and list users. Also add generic view to Retrieve, Update and Destroy an Account instance.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .serializers import AccountSerializer
+from .models import Account
+from .permissions import CreateUserOrIsColaborator, IsOwnerOrColaborator
 
-# Create your views here.
+
+class AccountView(ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [CreateUserOrIsColaborator]
+
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+
+class AccountDetailView(RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsOwnerOrColaborator]
+
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+    lookup_url_kwarg = "account_id"
