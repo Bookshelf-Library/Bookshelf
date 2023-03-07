7b3fd7905af64e636c39d5aab156c9b88fe547b2
Igorttdp <igorttdp.dev@email.com>
2023-03-07 12:35:54 -0300

    feat: add 'copies' field. Its value is returned by the get copies serializer Method

--- books/serializers.py
+++ books/serializers.py
@@ -1,12 +1,18 @@
 from rest_framework import serializers
 from .models import Book, Follow
+from copies.models import Copy
 
 
 class BookSerializer(serializers.ModelSerializer):
+    copies = serializers.SerializerMethodField()
+
     class Meta:
         model = Book
-        fields = ["id", "title", "pages", "author", "publisher", "account"]
-        read_only_fields = ["id", "account"]
+        fields = ["id", "title", "pages", "author", "publisher", "account", "copies"]
+        read_only_fields = ["id", "account", "copies"]
+
+    def get_copies(self, obj):
+        return Copy.objects.filter(book=obj).count()
 
 
 class FollowSerializer(serializers.ModelSerializer):
