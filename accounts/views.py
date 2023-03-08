f9592558dd0efeb47340bdf39c032e11a95a2539
rogeraguiar0 <aguiarroger02@gmail.com>
2023-03-08 10:22:09 -0300

    feat: start account status view

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView, Response
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from django.shortcuts import get_object_or_404
 from .serializers import AccountSerializer
 from .models import Account
 from .permissions import CreateUserOrIsColaborator, IsOwnerOrColaborator
@@ -22,3 +24,15 @@ class AccountDetailView(RetrieveUpdateDestroyAPIView):
     serializer_class = AccountSerializer
 
     lookup_url_kwarg = "account_id"
+
+
+class AccountStatusDetailView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsOwnerOrColaborator]
+
+    def get(self, request, account_id):
+        find_account = get_object_or_404(Account, pk=account_id)
+
+        serializer = AccountSerializer(find_account)
+
+        return Response(serializer.data, 200)
