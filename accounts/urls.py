9608b5634a0d3f35eebb558cf31ee4fec68c9f61
rogeraguiar0 <aguiarroger02@gmail.com>
2023-03-08 10:21:38 -0300

    feat: add account status url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import AccountView, AccountDetailView
+from .views import AccountView, AccountDetailView, AccountStatusDetailView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path("users/", AccountView.as_view()),
     path("login/", TokenObtainPairView.as_view()),
     path("refresh/", TokenRefreshView.as_view()),
-    path("users/<str:account_id>/", AccountDetailView.as_view())
+    path("users/<str:account_id>/", AccountDetailView.as_view()),
+    path("users/<str:account_id>/status/", AccountStatusDetailView.as_view())
 ]
