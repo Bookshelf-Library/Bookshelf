f703b57b643d8581bd8a086d180fca6ff9a9ec7b
Pedro Costa <pedrohsc100@gmail.com>
2023-03-09 13:23:36 -0300

    fix: applied the follow logic to be unique

--- books/views.py
+++ books/views.py
@@ -1,10 +1,11 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from .models import Book, Follow
 from .serializers import BookSerializer, FollowSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
 from .permissions import BookPermission
 from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 
 
 class ListCreateBooks(generics.ListCreateAPIView):
@@ -19,6 +20,16 @@ class FollowBook(generics.CreateAPIView):
     permission_classes = [IsAuthenticated]
     queryset = Follow.objects.all()
     serializer_class = FollowSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        find_account = Book.objects.filter(account=request.user)
+        if find_account:
+            return Response({"message": "user is already following this book"}, status=400)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     def perform_create(self, serializer):
         primary_key = self.kwargs["book_id"]
