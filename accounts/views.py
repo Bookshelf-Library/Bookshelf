b1afa4aa04c389a4054f716dc407ba697b577348
Igorttdp <igorttdp.dev@email.com>
2023-03-10 13:26:45 -0300

    feat: add AccountLoanView to loan a book copy
    The view has JWTAuthenticatin as authentication class, and IsAuthenticated & IsAdminUser as permissions_classes. The view override the 'create' method that is inhirited from CreateAPIView. The method in question makes the loan of the first available copy that is compatible with the id of the searched book.

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,21 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.views import APIView, Response
 from rest_framework.generics import (
     ListCreateAPIView,
     RetrieveUpdateDestroyAPIView,
     RetrieveAPIView,
+    CreateAPIView,
 )
 from django.shortcuts import get_object_or_404
 from .serializers import AccountSerializer
+from copies.serializers import LoanSerializer
 from .models import Account
+from copies.models import Copy, Loan
 from .permissions import CreateUserOrIsColaborator, IsOwnerOrColaborator
+import ipdb
+
+from datetime import timedelta, datetime
 
 
 class AccountView(ListCreateAPIView):
@@ -40,6 +46,42 @@ class AccountStatusDetailView(APIView):
         serializer = AccountSerializer(find_account)
 
         return Response(serializer.data, 200)
+
+
+class AccountLoanView(CreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+    queryset = Loan.objects.all()
+    serializer_class = LoanSerializer
+
+    def create(self, request, *args, **kwargs):
+        account_id = kwargs["account_id"]
+        book_id = kwargs["book_id"]
+
+        avaliable_copies = Copy.objects.filter(
+            book_id=book_id, is_avaliable=True
+        ).first()
+
+        if avaliable_copies is None:
+            return Response({"message": "No copy avaliable"})
+
+        avaliable_copies.is_avaliable = False
+
+        deliver = datetime.now() + timedelta(days=7)
+
+        loan = Loan(
+            account_id=account_id,
+            copy=avaliable_copies,
+            deliver_in=deliver,
+        )
+
+        avaliable_copies.last_loan = datetime.now()
+        avaliable_copies.save()
+        loan.save()
+
+        serializer = LoanSerializer(loan)
+        return Response(serializer.data, 201)
 
 
 class AccountLoansDetailView(RetrieveAPIView):
