fda301d26b65c97845bc2bc6730b712f7e2dc8b9
rogeraguiar0 <aguiarroger02@gmail.com>
2023-03-13 12:13:55 -0300

    feat: add drf-spectacular schema and doc urls

--- biblioteKA/urls.py
+++ biblioteKA/urls.py
@@ -13,12 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.contrib import admin 
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("accounts.urls")),
     path("api/", include("books.urls")),
     path("api/", include("copies.urls")),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
