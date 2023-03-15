4bfd198efdf6199a5d9f9b409b6386b84f201ef8
Igorttdp <igorttdp.dev@email.com>
2023-03-15 11:23:10 -0300

    feat: remove function 'is_punished' due change of logic. On the other hand, adds the function 'remove_punishment' for remove a punishment of an account

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,8 +1,10 @@
 import pytz
-from rest_framework.views import Response, status
 
 
-def permission_to_loan(account_id, Loan=None, current_date=None):
+def permission_to_loan(account_id, Loan=None, current_date=None, punishment=None):
+    if punishment:
+        return False
+
     if Loan is None:
         from copies.models import Loan
 
@@ -16,10 +18,10 @@ def permission_to_loan(account_id, Loan=None, current_date=None):
     loans = Loan.objects.filter(account_id=account_id, is_active=True)
 
     utc = pytz.UTC
-    expires_on = current_date.replace(tzinfo=utc)
+    expires_date = current_date.replace(tzinfo=utc)
 
     for loan in loans:
-        if loan.delivery_at is None and expires_on > loan.deliver_in:
+        if loan.delivery_at is None and expires_date > loan.deliver_in:
             return False
 
     return True
@@ -32,3 +34,9 @@ def openingtime(day, hour):
     if time > 18 or time < 9:
         return False
     return True
+
+
+def remove_punishment(*args):
+    account_punished = args[0]
+    account_punished.punishment = None
+    account_punished.save()
